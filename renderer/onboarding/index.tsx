3c1367055d8f49524446af2f103e0a26074a8cb6
Akhil Konduru <akhilkonduru23@gmail.com>
2026-09-13 16:01:56 -0500

    feat: restore onboarding flow

---
+++ renderer/onboarding/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { initLogging } from "../lib/utils";
+import { OnboardingView } from "./onboarding-view";
+import "../styles.css";
+
+initLogging();
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <OnboardingView />
+  </React.StrictMode>,
+);
+
+if (import.meta.hot) import.meta.hot.accept();
